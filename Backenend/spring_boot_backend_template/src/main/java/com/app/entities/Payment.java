3771de5fdff07e4597329f0ebae3f5632d037a7a
Prerna Gupta <prerna3151996@gmail.com>
2024-02-09 06:34:00 +0530

    Payment

--- Backenend/spring_boot_backend_template/src/main/java/com/app/entities/Payment.java
+++ Backenend/spring_boot_backend_template/src/main/java/com/app/entities/Payment.java
@@ -1,5 +1,34 @@
 package com.app.entities;
 
-public class Payment {
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="payment")
+@Getter
+@Setter
+public class Payment extends BaseEntity {
+	@Column(length = 50)
+	 private Long transactionId;
+	@Column(length=50)
+	 private String paymentStatus;
+      private LocalDate date;
+      
+	  private double totalAmount;
+
+		public Payment(Long transactionId, String paymentStatus, LocalDate date, double totalAmount) {
+			super();
+			this.transactionId = transactionId;
+			this.paymentStatus = paymentStatus;
+			this.date = date;
+			this.totalAmount = totalAmount;
+		}
+	    
 
 }
